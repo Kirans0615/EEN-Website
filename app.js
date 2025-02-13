334909e707197cab16314e5f73e9ed4954906613
Kiran Sen <166645524+Kirans0615@users.noreply.github.com>
2025-02-12 23:09:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, {
+        threshold: 0.5 // Trigger when 50% of the element is visible
+    });
+
+    document.querySelectorAll('.line').forEach(line => {
+        observer.observe(line);
+        line.style.transform = 'translateY(-100px)'; // Start position for animation
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const spans = document.querySelectorAll('.dynamic-weight');
+    let currentIndex = 0; // Starting index
+
+    function updateWeight() {
+        // Reset all to default
+        spans.forEach(span => span.classList.remove('bold'));
+        
+        // Set the current to bold
+        spans[currentIndex].classList.add('bold');
+
+        // Move to next index or cycle back to start
+        currentIndex = (currentIndex + 1) % spans.length;
+    }
+
+    // Initial call to set the first item's weight
+    updateWeight();
+
+    // Continue updating every 2 seconds
+    setInterval(updateWeight, 1500);
+});
+
+document.querySelectorAll('.benefit-title').forEach(button => {
+    button.addEventListener('click', function() {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, {
+        threshold: 0.5 // Trigger when 50% of the element is visible
+    });
+
+    document.querySelectorAll('.line').forEach(line => {
+        observer.observe(line);
+        line.style.transform = 'translateY(-100px)'; // Start position for animation
+    });
+});
+
+
+
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
@@ -5,7 +82,6 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
-
 
 const textElements = [
     "Sell Your Home",
@@ -20,6 +96,12 @@ const imagePaths = [
     "/PICS/family.jpg",
     "/PICS/bridge.jpg"
 ];
+const imageStyles = [
+    { height: '50px', objectFit: 'cover', width: '100%' },
+    { height: '50px', objectFit: 'contain', width: '100%' },
+    { height: '50px', objectFit: 'cover', width: '100%' },
+    { height: '50px', objectFit: 'contain', width: '100%' }
+];
 
 const textContainer = document.querySelector('.text-section');
 const imageElement = document.getElementById('dynamic-image');
@@ -27,10 +109,8 @@ const imageElement = document.getElementById('dynamic-image');
 let currentIndex = 0;
 
 function updateContent() {
-    // Fade out the image
     imageElement.style.opacity = 0;
 
-    // Wait for the fade effect to complete before changing the image source and text
     setTimeout(() => {
         textContainer.querySelectorAll('p').forEach((p, index) => {
             p.classList.remove('active');
@@ -40,24 +120,27 @@ function updateContent() {
         });
 
         imageElement.src = imagePaths[currentIndex];
-        imageElement.alt = textElements[currentIndex];  // Update alt attribute for accessibility
+        imageElement.alt = textElements[currentIndex];
 
-        // Fade in the new image
+        if (!imageElement.classList.contains('dynamic-image-style')) {
+            imageElement.classList.add('dynamic-image-style');
+        }
+
         imageElement.style.opacity = 1;
 
         currentIndex = (currentIndex + 1) % textElements.length;
-    }, 500);  // Duration of the fade-out effect, should match the CSS transition time
+    }, 500);
 }
 
-// Create text elements dynamically
 textElements.forEach(text => {
     const p = document.createElement('p');
     p.textContent = text;
     textContainer.appendChild(p);
 });
 
-// Initial highlight and image
 updateContent();
-
-// Change text and image every 4 seconds plus transition time
 setInterval(updateContent, 4500);
+
+// Adding functionality for alternating font weight
+
+
